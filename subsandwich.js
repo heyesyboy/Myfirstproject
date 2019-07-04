edcea5ce3a3605270dc6e7f5cd7a902c38b00ea9
codenation <codenation@codenations-MacBook-Pro.local>
2019-07-04 14:00:18 +0100

    change

--- subsandwich.js
+++ subsandwich.js
@@ -3,3 +3,10 @@ const takeOrder = (topping1, topping2, topping3, topping4, topping5) => {
 }
 takeOrder ("lettuce", "chicken", "tomato", "salami", "cucumber")
 takeOrder ("tomato", "cucumber", "peppers","sweetcorn", "cheese")
+
+
+const takeOrder = (topping1, topping2, topping3, topping4, topping5) => {
+    console.log(`Sub Sandwich with ${topping1}, ${topping2}, ${topping3}, ${topping4}, ${topping5}`);
+}
+takeOrder ("lettuce", "chicken", "tomato", "salami", "cucumber")
+takeOrder ("tomato", "cucumber", "peppers","sweetcorn", "cheese")
